8b159d8689ffe4469170c010e983bdcc0752f781
null <91502871+null8R@users.noreply.github.com>
2023-11-03 18:28:17 +0900

    完成

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@600&display=swap" rel="stylesheet">
-    <link href="src/css/main.css" rel="stylesheet">
-    
+    <link href="./src/css/main.css" rel="stylesheet">
+
     <title>こんざつなう！ / 3-2</title>
   </head>
 
@@ -20,7 +20,7 @@
     <div id="navbar">
       <nav class="navbar navbar-expand-sm navbar-light fixed-top glass-filter">
         <div class="container-fluid">
-          <a class="navbar-brand" href="">😊 こんざつなう！ <span class="subTxt">/ 3-2</span></a>
+          <a class="navbar-brand" href="/">😊 こんざつなう！ <span class="subTxt">/ 3-2</span></a>
 
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -29,10 +29,7 @@
           <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link" href="https://yakitori333.weebly.com/" target="_blank" rel="noopener noreferrer">1-2</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="staff/">Staff</a>
+                <a class="nav-link" href="./staff">Staff</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="https://github.com/null8r/konzatsu-now">README</a>
@@ -44,10 +41,37 @@
     </div>
 
     <main id="top" class="glass-filter">
+      <?php
+      //dbと接続
+      $db = 'mysql:host=ホスト名;dbname=データベース名;charset=utf8mb4';
+      $user = 'ユーザー名';
+      $passwd = 'パスワード';
+
+      $PDO = new PDO($db, $user, $passwd);
+
+      //sql、混雑状況の取得
+      $status_table = 'テーブル名';
+
+      $sql_status = "SELECT * FROM " . $status_table . ";";
+      $res_status = $PDO->query($sql_status);
+
+      //配列整理
+      $status = $res_status->fetchAll();
+      $status = $status[0]['status'];
+
+      //sql、取得した混雑状況を元にステータスとインフォ、枠の色を取得
+      $konzatsu_tabele = 'テーブル名';
+      
+      $sql_main = "SELECT * FROM " . $konzatsu_tabele . " WHERE id = ". $status . ";";
+      $stmt = $PDO->query($sql_main);
+
+      //配列整理
+      $data = $stmt->fetchAll();
+      ?>
+
       <div class="konzatsu">
-        <h1>本物は<a href="https://null8r.github.io/konzatsu">こっち</a>!</h1>
         <h1 class="boxTitle">
-          3年2組<br>
+          本館3階 3年2組<br>
           こんざつ状況
         </h1>
 
@@ -55,10 +79,10 @@
           最新情報を確認するには更新してください。
         </p>
 
-        <div class="status">
-          <h1>😴準備中</h1>
+        <div class="status" style="border: solid 10px <?php echo $data[0]['color']; ?>">
+          <h1><?php echo $data[0]['status']; ?></h1>
           <p>
-            まだ販売を開始していません。
+            <?php echo $data[0]['info']; ?>
           </p>
         </div>
 
@@ -68,22 +92,17 @@
       </div>
     </main>
 
-    <footer>
+    <footer class="glass-filter">
       <p>
         <span class="subTxt">わたしの<span style="color: #f0f;">コハル</span>ちゃんをかえして、、、</span><br>
-        Developed by <a href="https://github.com/null8r" title="GitHub" target="_blank" rel="noopener noreferrer">null8r</a>. 
+        Developed by <a href="https://github.com/null8r" title="GitHub" target="_blank" rel="noopener noreferrer">null8r🦋</a>. 
       </p>
     </footer>
-
-    <script src=""></script>
-
-    <noscript>
-      
-    </noscript>
 
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/twemoji@latest/dist/twemoji.min.js" crossorigin="anonymous"></script>
+    <script src="./src/js/main.js"></script>
   </body>
 </html>
--- staff/index.php
+++ staff/index.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@600&display=swap" rel="stylesheet">
     <link href="../src/css/main.css" rel="stylesheet">
     <link href="./src/css/staff.css" rel="stylesheet">
-    
+
     <title>こんざつなう！ / 3-2</title>
   </head>
 
@@ -26,11 +26,11 @@
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-    
+
           <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link" href="../">Home</a>
+                <a class="nav-link" href="/">Home</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="https://github.com/null8r/konzatsu-now">README</a>
@@ -48,43 +48,35 @@
           こんざつ状況を選択
         </h1>
 
-        <form name="list" id="list">
-          <label><input type="radio" name="status" value="status/closed" class="checkbox" checked="">😴準備中</label>
-          <label><input type="radio" name="status" value="status/empty" class="checkbox" checked="">😄空いている</label>
-          <label><input type="radio" name="status" value="status/little" class="checkbox" checked="">😧ちょい混み</label>
-          <label><input type="radio" name="status" value="status/crowded" class="checkbox" checked="">😵げき混み</label>
-          <label><input type="radio" name="status" value="status/sold" class="checkbox" checked="">😫完売</label>
+        <form name="list" id="list" method="post" action="send.php">
+          <label class="closed"><input type="radio" name="status" value="1" class="checkbox">😴準備中</label>
+          <label class="empty"><input type="radio" name="status" value="2" class="checkbox">😄空いている</label>
+          <label class="little"><input type="radio" name="status" value="3" class="checkbox">😧ちょい混み</label>
+          <label class="crowded"><input type="radio" name="status" value="4" class="checkbox">😵げき混み</label>
+          <label class="sold"><input type="radio" name="status" value="5" class="checkbox">😫完売</label>
+          <label class="stop"><input type="radio" name="status" value="6" class="checkbox">🥺一時販売停止</label>
+          <label class="only"><input type="radio" name="status" value="7" class="checkbox">🧋タピオカのみ</label>
+          <input type="submit" value="更新">
         </form>
     </main>
 
     <footer class="glass-filter">
       <p>
-        <?php
-            try 
-            {   
-                $database_handler = new PDO('mysql:host=localhost;dbname=konzatsunow_konzatsu;charset=utf8mb4', 'konzatsunow_db', '73YhLRcXHhY9hgK');
-             }   
-             catch (PDOException $e) 
-             {   
-                 echo "DB接続に失敗しました。\n";
-                 echo $e->getMessage() . "\n";
-                 exit;
-             }   
-        ?>
         <span class="subTxt">わたしの<span style="color: #f0f;">コハル</span>ちゃんをかえして、、、</span><br>
-        Developed by <a href="https://github.com/null8r" title="GitHub" target="_blank" rel="noopener noreferrer">null8r</a>.
+        Developed by <a href="https://github.com/null8r" title="GitHub" target="_blank" rel="noopener noreferrer">null8r🦋</a>.
       </p>
     </footer>
 
     <script src=""></script>
 
     <noscript>
-      
+
     </noscript>
 
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/twemoji@latest/dist/twemoji.min.js" crossorigin="anonymous"></script>
+    <script src="../src/js/main.js"></script>
   </body>
 </html>
